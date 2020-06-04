cd6a3c3eb0e4138fc75b4332c0b80c40403d33b0
jtmat <jtmat@DESKTOP-8U20PPL>
2020-06-04 14:01:54 -0400

    Movie stats

--- MovieStats/src/moviestats/MovieStats.java
+++ MovieStats/src/moviestats/MovieStats.java
@@ -7,6 +7,7 @@ package moviestats;
 
     import java.io.*;
     import java.util.Scanner;
+    import java.util.InputMismatchException;
 /**
  *
  * @author jtmat
@@ -15,14 +16,15 @@ package moviestats;
 public class MovieStats {
     private Scanner x;
     
+    //mainmenu display
     public static void MainMenu(){
         //THE MAIN MENU OF MY PROGRAM
         System.out.println("Input number for program");
-        System.out.println("1. Rate movies! ");
-        System.out.println("2. Add movie to our system!");
+        System.out.println("1. See movies in our data base! ");
+        System.out.println("2. Add movie to our system and rate it! ");
     }
     
-    
+    //opening the file
     public void openFile(){
         try {
             x = new Scanner(new File("MovieStat.dat"));
@@ -30,7 +32,7 @@ public class MovieStats {
             System.out.println("File not found");
         }     
     }
-    
+    //read file
     public void readFile(){
         while(x.hasNext()){
             String MovieTitle = x.next();
@@ -39,85 +41,37 @@ public class MovieStats {
             System.out.printf("%s %s %s \n", MovieTitle, MovieYear, MovieScore + "% ");
         }
     }
-    
-    public void Array(){
-        int movieTotal = 16;
-        int i = 0;
-        String [] Movies = new String [movieTotal];
-        while(x.hasNext()){
-            i++;
-            String MovieTitle = x.next();
-            String MovieYear = x.next();
-            String MovieScore = x.next();
-            Movies[i] = MovieTitle;
-        }
-        System.out.println(movieTotal);
-        //System.out.println(Movies[line]); 
-    }
-    
-    
-    public static String[] readArray(String MoviesStats){
-        int j = 0;
-        try{
-            Scanner s1 = new Scanner(new File("MovieStats.dat"));
-            while(s1.hasNextLine()){
-                j++;
-            }
-            String [] Movie = new String[j];
-            Scanner s2 = new Scanner(new File("MovieStats.dat"));
-            for(int i = 0; i < j; i++){
-                Movie[i] = s2.next();
-            }
-            
-        }catch(FileNotFoundException e){
-            
-        }
-        
-        return null;
-    }
+    //close file
     public void closeFile(){
         x.close();
     }
-    
-    public void newScore(){
-        File dataFile = new File("MovieStat.dat");
-        FileWriter out;
-	BufferedWriter writeFile;
-	Scanner keyedInput = new Scanner(System.in);
-        int score;
-	try {
-            out = new FileWriter(dataFile,true);
-            writeFile = new BufferedWriter(out);
-            score = keyedInput.nextInt();
-            writeFile.write(String.valueOf(score));	
-            writeFile.close();
-            out.close();
-            System.out.println("Data written to file.");
-    	} catch (IOException e) {
-            System.out.println("Problem writing to file.");
-            System.err.println("IOException: " + e.getMessage());
-    	}
-    }
-    
+
+    //wrting movie to file
     public void writeFile(){
+        boolean success = false;
         File dataFile = new File("MovieStat.dat");
         FileWriter out;
 	BufferedWriter writeFile;
 	Scanner keyedInput = new Scanner(System.in);
 	double score;
+        int year = 0;
 	String name;
 	try {
             out = new FileWriter(dataFile,true);
             writeFile = new BufferedWriter(out);
             for (int i = 0; i < 1; i++) {
-                System.out.print("Enter movie: ");
+                System.out.print("Enter movie(dashs as spaces): ");
                 name = keyedInput.next();
+                System.out.println("Enter year film came out: ");
+                year = keyedInput.nextInt();
+                System.out.println("that is not a year");
                 System.out.print("Enter score: ");
                 score = keyedInput.nextInt();
                 writeFile.write(name + " ");
+                writeFile.write("(" + String.valueOf(year) + ") ");
                 writeFile.write(String.valueOf(score));
                 writeFile.newLine();
-            }  	
+            }  	//write movie, year and score to java
             writeFile.close();
             
             out.close();
@@ -140,26 +94,23 @@ public class MovieStats {
         
         MovieStats r = new MovieStats();
         Scanner keyedInput = new Scanner (System.in);
-        
-	MainMenu();
-        
+
         
         while(input<3){
+            MainMenu();
             System.out.println("Input: ");
             input = keyedInput.nextInt(); 
             if(input == 2){
+                //Add movie to database
                 r.writeFile();
             }
             
             else if(input == 1){
+                //showing movies in database
                 try{
                     System.out.println("");
                     r.openFile();
                     r.readFile();
-                    System.out.println("Choose movie to rate");
-                    line = keyedInput.nextInt();
-                    String [] Movie = readArray("MovieStats.dat");
-                    System.out.println(Movie[line] + "HI");
                     r.closeFile();
                 }catch(Exception e){
                     System.out.println("");
